80dc5e12546b83efcd968e5c672f7c0eabda5966
comsavvy <olusolaogundepo@gmail.com>
2020-12-13 23:54:28 +0100

    All countries were scrapped
    I also explained the function of each method in the class using
    DOCSTRING

--- features/spiders/details_extract.py
+++ features/spiders/details_extract.py
@@ -4,9 +4,22 @@ import scrapy
 class DetailsExtractSpider(scrapy.Spider):
     name = 'details_extract'
     allowed_domains = ['behindthename.com']
-    start_urls = ['http://www.behindthename.com/submit/names/usage/eastern-african/']
+    start_urls = ['http://www.behindthename.com/submit/names/usage/eastern-african/']        
 
     def parse(self, response):
+        """
+            Getting all the countries URL,
+            And redirecting each country to 'each_page' method to confirm the pages,
+            And then scrap them in 'page_details' method.
+        """
+        all_country = response.css('div.nb-quickfilter > select[name=usage] > option ::attr(value)').extract()[1:-1]
+        for country in all_country:
+            yield response.follow(url=response.urljoin(country), callback=self.each_page)
+    
+    def each_page(self, response):
+        """
+            Confirming the number of pages and also redirecting them to be scrapped.
+        """
         links = len(set(response.css('nav.pagination > a::attr(href)').extract()))
         i = 0
         while i <= links:
@@ -14,6 +27,9 @@ class DetailsExtractSpider(scrapy.Spider):
             i += 1
             
     def page_details(self, response):
+        """
+            Getting all the information needed from all the pages here.
+        """
          general = response.css("div.browsename")
          for gen in general:
             using = gen.xpath('.//span//text()').extract()
